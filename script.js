a9c09e36e867c21fa1d45317fd7d05c63d6d2cab
DonnaThompson7 <130340823+DonnaThompson7@users.noreply.github.com>
2023-05-02 21:54:02 -0400

    fix background image size, make code more readable and add comments

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
 var charsLower = "abcdefghijklmnopqrstuvwxyz";
 var charsUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var charsNumeric = "0123456789";
-// on link for OWASP, I chose the list (subset) accepted by MS/Oracle: https://docs.oracle.com/cd/E11223_01/doc.910/e11197/app_special_char.htm
-var charsSpecial = "@%+\/'!#$^?:,(){}[]~-_.";
+var charsSpecial = "@%+\/'!#$^?:,(){}[]~-_.";     // on link for OWASP, I chose the list/subset accepted by MS/Oracle
 
 function generatePassword() {
-    var pwdLength = 8;
+    var pwdLength;
     var useLowercase = false;
     var useUppercase=false;
     var useNumeric=false;
     var useSpecialChars=false;
     var charsToUse = ""; 
     var newPassword = "";
+    var invalidEntry;
 
 
-    //User is prompted to choose length of password. Defaults to 8 chars. 
+    //User is prompted to choose length of password.
         pwdLength = window.prompt("Enter desired length of password. (Note that the length must be a number between 8 and 128): ");
         //includes VALIDATION: will prompt user until LENGTH IS BE BETWEEN 8 and 128
         while (pwdLength < 8 || pwdLength > 128) {
           pwdLength = window.prompt("*INVALID ENTRY* The length must be a number between 8 and 128. Please re-enter desired length of password: ");
         } 
        
-      var invalidEntry;
-      do  {
+        do  {
             //User is prompted to specify 4 char types for password
+
             // 1 - specify if want to use lowercase. Defaults to true.
             useLowercase = window.confirm("Include lowercase? (Yes/No) ");
+
             // 2 - specify if want to use uppercase. Defaults to true.
             useUppercase = window.confirm("Include uppercase? (Yes/No) ");
+
             // 3 - specify if want to use numeric values. Defaults to true.
             useNumeric = window.confirm("Include numeric values? (Yes/No) ");
+
             // 4 - specify if want to use special chars. Defaults to true.
             useSpecialChars = window.confirm("Include special chars? (Yes/No) ");
+
             //VALIDATION: if no character type has been selected, alert user and do over.
             if (!useLowercase && !useUppercase && !useNumeric && !useSpecialChars) 
                 {
@@ -42,33 +46,32 @@ function generatePassword() {
           }
         while (invalidEntry);
 
-    console.log("newPassword.length = " + newPassword.length)
-    //build string of chars to use for this pwd, per user input above
+    //build one string of all chars to use for the random pwd generator
     //also add 1 of each selected char type, to ensure pwd contains at least 1 of each type
         if (useLowercase)    {
-            charsToUse = charsLower;
-            var randomNumber = Math.floor(Math.random() * charsLower.length);   //new
-            newPassword += charsLower.substring(randomNumber, randomNumber +1); //new
-            }
-        if (useUppercase)    {
-            charsToUse = charsToUse + charsUpper;
-            var randomNumber = Math.floor(Math.random() * charsUpper.length);   //new
-            newPassword += charsUpper.substring(randomNumber, randomNumber +1); //new
-            }
-        if (useNumeric)      {
-            charsToUse = charsToUse + charsNumeric;
-            var randomNumber = Math.floor(Math.random() * charsNumeric.length);   //new
-            newPassword += charsNumeric.substring(randomNumber, randomNumber +1); //new
-            }
-        if (useSpecialChars) {
-            charsToUse = charsToUse + charsSpecial;
-            var randomNumber = Math.floor(Math.random() * charsSpecial.length);   //new
-            newPassword += charsSpecial.substring(randomNumber, randomNumber +1); //new
-            }
-      console.log("after inserting 1 each of char type, newPassword = " + newPassword + " and length = " + newPassword.length)
+          charsToUse = charsLower;
+          var randomNumber = Math.floor(Math.random() * charsLower.length);   
+          newPassword += charsLower.substring(randomNumber, randomNumber +1); 
+          }
+      if (useUppercase)    {
+          charsToUse = charsToUse + charsUpper;
+          var randomNumber = Math.floor(Math.random() * charsUpper.length);   
+          newPassword += charsUpper.substring(randomNumber, randomNumber +1); 
+          }
+      if (useNumeric)      {
+          charsToUse = charsToUse + charsNumeric;
+          var randomNumber = Math.floor(Math.random() * charsNumeric.length);   
+          newPassword += charsNumeric.substring(randomNumber, randomNumber +1); 
+          }
+      if (useSpecialChars) {
+          charsToUse = charsToUse + charsSpecial;
+          var randomNumber = Math.floor(Math.random() * charsSpecial.length);   
+          newPassword += charsSpecial.substring(randomNumber, randomNumber +1); 
+          }
+             
+    console.log("after inserting 1 each of char type, newPassword = " + newPassword + " and length = " + newPassword.length)
 
-    // generate rest of new password for user-specified length and character set, using Math.random and Math.floor
-    //  starting i = newPassword.length, while < pwdLength
+    // generate rest of new password for user-specified length and char type(s), using Math.random and Math.floor
       for (var i = newPassword.length; i < pwdLength; i++) {
         var randomNumber = Math.floor(Math.random() * charsToUse.length);
         newPassword += charsToUse.substring(randomNumber, randomNumber +1);
